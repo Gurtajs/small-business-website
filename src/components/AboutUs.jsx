1aa85b61b7c00711d9e8186b3129f27b7e39e881
Gurtaj <gurtajs98@gmail.com>
2024-06-28 23:08:56 +0100

    removed google map

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import RenderMap from './renderMap'
+// import RenderMap from './renderMap'
 
 const AboutUs = () => {
   
   return (
     <div className='container-aboutus'>
       <h1 className='header-aboutus'>About Us</h1>
-      <RenderMap/>
+      {/* <RenderMap/> */}
     </div>
   )
 }
--- src/components/RenderMap.jsx
+++ src/components/RenderMap.jsx
@@ -1,30 +1,30 @@
-import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
-import { useMemo } from "react";
+// import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
+// import { useMemo } from "react";
 
-import React from 'react'
+// import React from 'react'
 
-const RenderMap = () => {
-  const { isLoaded }  = useLoadScript({
-      googleMapsApiKey: import.meta.env.VITE_REACT_APP_GOOGLE_API_KEY,
-  });
-const center = useMemo(() => ({ lat: 52.57556, lng: -2.03396 }), []);
+// const RenderMap = () => {
+//   const { isLoaded }  = useLoadScript({
+//       googleMapsApiKey: import.meta.env.VITE_REACT_APP_GOOGLE_API_KEY,
+//   });
+// const center = useMemo(() => ({ lat: 52.57556, lng: -2.03396 }), []);
 
-  return (
-    <div>
-      {!isLoaded ? (
-          <h1>Loading...</h1>
-      ) : (
-          <GoogleMap
-          mapContainerClassName="map-container"
-          center={center}
-          zoom={20}
+//   return (
+//     <div>
+//       {!isLoaded ? (
+//           <h1>Loading...</h1>
+//       ) : (
+//           <GoogleMap
+//           mapContainerClassName="map-container"
+//           center={center}
+//           zoom={20}
           
-          >
-          <MarkerF position={{ lat: 52.57556, lng: -2.03396 }}/>
-          </GoogleMap>
-      )}
-    </div>
-  )
-}
+//           >
+//           <MarkerF position={{ lat: 52.57556, lng: -2.03396 }}/>
+//           </GoogleMap>
+//       )}
+//     </div>
+//   )
+// }
 
-export default RenderMap
+// export default RenderMap
